46ffb4699c48d48cadd18d52c2e095ba5384d914
Joovski <53435623+Joovski@users.noreply.github.com>
2021-12-07 14:59:37 +0100

    .

--- Développement Front/fonctions.js
+++ Développement Front/fonctions.js
@@ -3,6 +3,7 @@ function check(form) {
     
 if(form.identifiant.value == "admin" && form.motdepasse.value == "admin") {
         window.open('index.html')
+        window.close('connexion.html')
 } else {
         alert("Mot de Passe ou Identifiant Incorrect")
        }
@@ -11,4 +12,10 @@ if(form.identifiant.value == "admin" && form.motdepasse.value == "admin") {
 // Deconnexion
 function deconnexion(form) {
         window.open('connexion.html')
+        window.close('index.html')
+}
+
+// Rafraichir la page
+function rafraichir(form) {
+        location.reload();
 }
