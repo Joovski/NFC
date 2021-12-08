d6e0db8dd587cf8f71d95805a8f5a09f813b525f
Joovski <53435623+Joovski@users.noreply.github.com>
2021-12-08 11:39:49 +0100

    .

--- Développement Front/nfc.php
+++ Développement Front/nfc.php
@@ -2,7 +2,7 @@
     // Connexion à une BDD hébergé par XAMPP
     $mysqli = new mysqli('localhost','root','','nfcreader');
 
-    // Récupération d'information
+    // Récupération d'information du NFC
     $req = "select * from nfcreader";
     echo ($req);
 ?>
