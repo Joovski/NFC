9cab5f13c60c2a0bd8efd6f143a0893126973b52
Joovski <53435623+Joovski@users.noreply.github.com>
2021-12-08 15:40:56 +0100

    .

--- Développement BackEnd/Node JS/mainHistorique.js
+++ Développement BackEnd/Node JS/mainHistorique.js
@@ -9,18 +9,16 @@ function createWindow(){
 
 app.whenReady().then(createWindow);
 
-const Devices = smartcard.Devices;
-const devices = new Devices();
-devices.on("device-activated", DetectionCarte);
+  const Devices = smartcard.Devices;
+  const devices = new Devices();
+  devices.on("device-activated", DetectionCarte);
 
 function DetectionCarte(event){
-    const currentDevices = event.devices;
-    var device = event.device;
-    device.on("card-inserted", CarteConnecte);
+  const currentDevices = event.devices;
+  var device = event.device;
+  device.on("card-inserted", CarteConnecte);
 }
 
 function RecuperationId(event){
-  
+
 }
-
-
